a0b57ae4a79ed78623f5ebba4479258764e719cc
josh <joshuaholman5@gmail.com>
2025-05-20 12:20:13 -0700

    comment out for compilation

--- jlox/app/src/main/java/lox/Parser.java
+++ jlox/app/src/main/java/lox/Parser.java
@@ -11,58 +11,58 @@ class Parser {
     this.tokens = tokens;
   }
 
-  private Expr expression() {
-    return equality();
-  }
+  // private Expr expression() {
+  //   return equality();
+  // }
 
-  private Expr equality() {
-    Expr expr = comparison();
-    while (match(BANG_EQUAL, EQUAL_EQUAL)) {
-      Token operator = previous();
-      Expr right = comparison();
-      expr = new Expr.Binary(expr, operator, right);
-    }
+  // private Expr equality() {
+  //   Expr expr = comparison();
+  //   while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+  //     Token operator = previous();
+  //     Expr right = comparison();
+  //     expr = new Expr.Binary(expr, operator, right);
+  //   }
+  //
+  //   return expr;
+  // }
 
-    return expr;
-  }
-
-  private Expr comparison() {
-    Expr expr = term();
-
-    while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
-      Token operator = previous();
-      Expr right = term();
-      expr = new Expr.Binary(expr, operator, right);
-    }
-  }
+  // private Expr comparison() {
+  //   Expr expr = term();
+  //
+  //   while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+  //     Token operator = previous();
+  //     Expr right = term();
+  //     expr = new Expr.Binary(expr, operator, right);
+  //   }
+  // }
 
   /**
    * Addition and subraction
    */
-  private Expr term() {
-    Expr expr = factor();
-
-    while (match(MINUS, PLUS)) {
-      Token operator = previous();
-      Expr right = factor();
-      expr = new Expr.Binary(expr, operator, right);
-    }
-  }
+  // private Expr term() {
+  //   Expr expr = factor();
+  //
+  //   while (match(MINUS, PLUS)) {
+  //     Token operator = previous();
+  //     Expr right = factor();
+  //     expr = new Expr.Binary(expr, operator, right);
+  //   }
+  // }
 
   /**
    * Multiplication and division
    */
-  private Expr factor() {
-    Expr expr = unary();
-
-    while (match(SLASH, STAR)) {
-      Token operator = previous();
-      Expr right = unary();
-      expr = new Expr.Binary(expr, operator, right);
-    }
-
-    return expr;
-  }
+  // private Expr factor() {
+  //   Expr expr = unary();
+  //
+  //   while (match(SLASH, STAR)) {
+  //     Token operator = previous();
+  //     Expr right = unary();
+  //     expr = new Expr.Binary(expr, operator, right);
+  //   }
+  //
+  //   return expr;
+  // }
 
 
 
