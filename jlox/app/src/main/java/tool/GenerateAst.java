66c4d8e1349085a47140ad82e2fa023c7926c81a
Josh Holman <josh@jolman.me>
2025-06-18 12:19:14 -0700

    add grammar rules

--- jlox/app/src/main/java/tool/GenerateAst.java
+++ jlox/app/src/main/java/tool/GenerateAst.java
@@ -22,13 +22,16 @@ public class GenerateAst {
         "Binary : Expr left, Token operator, Expr right",
         "Grouping : Expr expression",
         "Literal : Object value",
-        "Unary : Token operator, Expr right"));
+        "Unary : Token operator, Expr right",
+        "Variable : Token name"));
 
     defineAst(outputDir, "Stmt", Arrays.asList(
         "Expression : Expr expression",
-        "Print : Expr expression"));
+        "Print : Expr expression",
+        "Var : Token name, Expr initializer"));
 
-    System.out.println("file '" + outputDir + "/Expr.java' successfully generated.");
+    System.out.println("File '" + outputDir + "/Expr.java' successfully generated.");
+    System.out.println("File '" + outputDir + "/Stmt.java' successfully generated.");
   }
 
   private static void defineAst(
