d1d63f6fa3b239ccbe0f986f34a14588db174a25
Josh Holman <josh@jolman.me>
2025-06-23 17:07:06 -0700

    progress

---
+++ jlox/app/src/main/java/lox/Environment.java
@@ -0,0 +1,12 @@
+package lox;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Environment {
+  private final Map<String, Object> values = new HashMap<>();
+
+  void define(String name, Object value) {
+    values.put(name, value);
+  }
+}
