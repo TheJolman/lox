ad5b19a59599b1603d2904b775fee64dc5770c12
Josh Holman <josh@jolman.me>
2025-07-01 14:18:18 -0700

    finish var statement

--- jlox/app/src/main/java/lox/Interpreter.java
+++ jlox/app/src/main/java/lox/Interpreter.java
@@ -12,6 +12,7 @@ class RuntimeError extends RuntimeException {
 }
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+  private Environment environment = new Environment();
 
   void interpret(List<Stmt> statements) {
     try {
@@ -43,6 +44,11 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     // unreachable
     return null;
+  }
+
+  @Override
+  public Object visitVariableExpr(Expr.Variable expr) {
+    return environment.get(expr.name);
   }
 
   private void checkNumberOperand(Token operator, Object operand) {
@@ -115,6 +121,16 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   public Void visitPrintStmt(Stmt.Print stmt) {
     Object value = evaluate(stmt.expression);
     System.out.println(stringify(value));
+    return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Stmt.Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+    environment.define(stmt.name.lexeme, value); // set to nil (using Java's null)
     return null;
   }
 
